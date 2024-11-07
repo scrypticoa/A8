a6fbf84affafcd6ef6ce7b32758f84bd9e946ee9
scrypticoa <scrypticoa@gmail.com>
2024-11-07 15:45:47 -0500

    stuff

--- src/Huffman.java
+++ src/Huffman.java
@@ -14,6 +14,16 @@ abstract class ABranch {
   int getChild(int node) {
     return (node - 1) / 2;
   }
+  
+  public abstract int value();
+  
+  public boolean isLessThan(ABranch other) {
+    return other.isGreaterThanValue(this.value());
+  }
+  
+  public boolean isGreaterThanValue(int value) {
+    return this.value() > value;
+  }
 }
 
 class Leaf extends ABranch {
@@ -23,6 +33,10 @@ class Leaf extends ABranch {
   public Leaf(String letter, Integer frequency) {
     this.letter = letter;
     this.frequency = frequency;
+  }
+  
+  public int value() {
+    return frequency;
   }
   
   public void addToForest(int start, Forest forest) {
@@ -35,7 +49,16 @@ class Forest extends ABranch {
   Integer total;
   
   public Forest(ABranch branch1, ABranch branch2) {
+    this.total = branch1.value() + branch2.value();
     
+    leaves = new ArrayList<Leaf>();
+    
+    branch1.addToForest(1, this);
+    branch2.addToForest(2, this);
+  }
+  
+  public int value() {
+    return total;
   }
   
   public void insert(Leaf leaf, int index) {
@@ -67,6 +90,8 @@ class Huffman {
   ArrayList<String> letters;
   ArrayList<Integer> frequencies;
   
+  Forest cypher;
+  
   public Huffman(ArrayList<String> letters, ArrayList<Integer> frequencies) {
     
     if (letters.size() != frequencies.size()) {
@@ -79,5 +104,58 @@ class Huffman {
     
     this.letters = letters;
     this.frequencies = frequencies;
+    
+    ArrayList<ABranch> branches = generateBranchArray(letters, frequencies);
+    
+    this.cypher = mergeAll(branches);
+  }
+  
+  public ArrayList<ABranch> generateBranchArray(ArrayList<String> letters, ArrayList<Integer> frequencies) {
+    
+    ArrayList<ABranch> result = new ArrayList<ABranch>();
+    
+    for (int i = letters.size() - 1; i > -1; i--) {
+      Leaf leaf = new Leaf(letters.get(i), frequencies.get(i));
+      
+      sortInto(result, leaf);
+    }
+    
+    return result;
+  }
+  
+  public void sortInto(ArrayList<ABranch> branches, ABranch newLeaf) {
+    for (int i = 0; i < branches.size(); i++) {
+      if (!branches.get(i).isLessThan(newLeaf)) {
+        branches.add(i, newLeaf);
+        return;
+      }
+    }
+    branches.add(newLeaf);
+  }
+  
+  public Forest mergeAll(ArrayList<ABranch> branches) {
+    while (branches.size() > 2) {
+      ABranch small0 = branches.remove(0);
+      ABranch small1 = branches.remove(0);
+      
+      sortInto(branches, new Forest(small0, small1));
+    }
+    
+    ABranch small0 = branches.remove(0);
+    ABranch small1 = branches.remove(0);
+    
+    return new Forest(small0, small1);
+  }
+  
+  public ArrayList<Boolean> encode(String toEncode) {
+    ArrayList<Boolean> encoded;
+    for (int i = 0; i < toEncode.length(); i++) {
+      encoded.add(null);
+    }
+    
+  }
+  
+  public void appendEncodeLetter(String letter, ArrayList<Boolean> output) {
+    
   }
 }
